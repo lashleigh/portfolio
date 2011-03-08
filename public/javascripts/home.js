a967126966ba34960d2d180840fc45bd900c79d3
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-03-08 15:58:08 -0800

    Added some logic to make show/hide all a little smarter. If everything gets hidden manually then it automatically switches the text to say show all rather than hide all.

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,17 +1,18 @@
+var number_of_questions;
+var number_visible;
 $(function() {
+  number_of_questions = $(".question").length;
+  number_visible = $(".answer:visible").length;
+
   $("code").addClass("prettyprint");
   prettyPrint();
 
   $("#toggle_all").bind("click", function() {
     var which = $(this).text();
     if(which == "Show all") {
-      $(".answer").slideDown(); 
-      $(this).html("Hide all");
-      $(".question").css("cursor", "n-resize");
+      show_all();
     } else if(which == "Hide all") {
-      $(".answer").slideUp(); 
-      $(this).html("Show all");
-      $(".question").css("cursor", "s-resize");
+      hide_all();
     }
   });
   $(".question").bind("click", function() {
@@ -20,9 +21,35 @@ $(function() {
       if(answer.is(":visible")) {
         answer.slideUp('fast');
         question.css("cursor", "s-resize");
+        decrement_visible();
       } else {
         answer.slideDown('fast');
         question.css("cursor", "n-resize");
+        increment_visible();
       }
   });
 });
+function show_all() {
+  number_visible = number_of_questions;
+  $(".answer").slideDown(); 
+  $("#toggle_all").html("Hide all");
+  $(".question").css("cursor", "n-resize");
+}
+function hide_all() {
+  number_visible = 0;
+  $(".answer").slideUp(); 
+  $("#toggle_all").html("Show all");
+  $(".question").css("cursor", "s-resize");
+}
+function increment_visible() {
+  number_visible++;
+  if(number_visible == number_of_questions) {
+    $("#toggle_all").html("Hide all"); 
+  }
+}
+function decrement_visible() {
+  number_visible--;
+  if(number_visible == 0) {
+    $("#toggle_all").html("Show all"); 
+  }
+}
