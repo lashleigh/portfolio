fe7e52f0eb26c525b3292e3c1626f2f34e6af675
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-03-08 00:12:55 -0800

    All the default files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '4355ba8a65e2be2d0f18ad326e8ba4c72e76cd8f0b3f1afe35f989e41f07cf28a2e028350667aecd6cc73ed9cfacb8284473a32f1477987b2fafdaad1faf3145'
---
+++ public/javascripts/home.js
@@ -0,0 +1,13 @@
+$(function() {
+  $(".question").bind("click", function() {
+      var question = $(this);
+      var answer = $(this).next();
+      if(answer.is(":visible")) {
+        answer.slideUp('fast');
+        question.css("cursor", "s-resize");
+      } else {
+        answer.slideDown('fast');
+        question.css("cursor", "n-resize");
+      }
+  });
+});
