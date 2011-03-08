c74c393bb0c12c8d7d7471c86361688af5033dd2
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-03-08 15:43:15 -0800

    Show/hide all link and toggle visibility

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,6 +2,18 @@ $(function() {
   $("code").addClass("prettyprint");
   prettyPrint();
 
+  $("#toggle_all").bind("click", function() {
+    var which = $(this).text();
+    if(which == "Show all") {
+      $(".answer").slideDown(); 
+      $(this).html("Hide all");
+      $(".question").css("cursor", "n-resize");
+    } else if(which == "Hide all") {
+      $(".answer").slideUp(); 
+      $(this).html("Show all");
+      $(".question").css("cursor", "s-resize");
+    }
+  });
   $(".question").bind("click", function() {
       var question = $(this);
       var answer = $(this).next();
