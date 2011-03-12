49e4463f3e7538e9e9c80f8d45163b66e03d9881
Ashleigh <lash@angua.local>
2011-03-11 23:20:05 -0600

    ordering networks by position and selecting only primary.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @about = Faq.find_by_question('about')
     @faqs = Faq.find(:all, :order => 'position ASC', :conditions => {:visible => true}) - @about.to_a
-    @networks = Network.find(:all)
+    @primary = Network.find(:all, :order => 'position ASC', :conditions => {:which => 'primary'})
   end
 
 end
