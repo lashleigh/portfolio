4d15014ccb25b9c1f595e7e4d99735dc14f9e2c7
Ashleigh Baumgardner <labaumgardner@gmail.com>
2011-10-23 01:37:31 -0700

    Very basic tab completion with a regex that splits and compares everthing on the current slide and in the parent javascript.

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,7 +1,8 @@
-function autocomplete(editor) {  // Minimal event-handling wrapper.
+function autocomplete(editor, show) {  // Minimal event-handling wrapper.
   editor.setOption('onKeyEvent', function(i, e) {
     // Hook into ctrl-space
-    if (e.keyCode == 32 && (e.ctrlKey || e.metaKey) && !e.altKey) {
+    //if (e.keyCode == 32 && (e.ctrlKey || e.metaKey) && !e.altKey) {
+    if (e.keyCode === 9) {
       e.stop();
       return startComplete();
     }
@@ -31,9 +32,15 @@ function autocomplete(editor) {  // Minimal event-handling wrapper.
     // We want a single cursor position.
     if (editor.somethingSelected()) return;
     // Find the token at the cursor
-    var cur = editor.getCursor(false), token = editor.getTokenAt(cur), tprop = token;
+    var cur = editor.getCursor(false);
+    var token = editor.getTokenAt(cur); 
+    var tprop = token;
+    console.log(cur, token, tprop);
+
     // If it's not a 'word-style' token, ignore the token.
     if (!/^[\w$_]*$/.test(token.string)) {
+      //token.string = token.string.replace(/^./, '');
+      //token.start += 1;
       token = tprop = {start: cur.ch, end: cur.ch, string: "", state: token.state,
                        className: token.string == "." ? "property" : null};
     }
@@ -42,7 +49,7 @@ function autocomplete(editor) {  // Minimal event-handling wrapper.
       tprop = editor.getTokenAt({line: cur.line, ch: tprop.start});
       if (tprop.string != ".") return;
       tprop = editor.getTokenAt({line: cur.line, ch: tprop.start});
-      if (!context) var context = [];
+      if (!context) { var context = []; }
       context.push(tprop);
     }
     var completions = getCompletions(token, context);
@@ -60,7 +67,7 @@ function autocomplete(editor) {  // Minimal event-handling wrapper.
     // Opera doesn't move the selection when pressing up/down in a
     // multi-select, but it does properly support the size property on
     // single-selects, so no multi-select is necessary.
-    if (!window.opera) sel.multiple = true;
+    if (!window.opera) { sel.multiple = true; }
     for (var i = 0; i < completions.length; ++i) {
       var opt = sel.appendChild(document.createElement("option"));
       opt.appendChild(document.createTextNode(completions[i]));
@@ -72,8 +79,9 @@ function autocomplete(editor) {  // Minimal event-handling wrapper.
     complete.style.top = pos.yBot + "px";
     document.body.appendChild(complete);
     // Hack to hide the scrollbar.
-    if (completions.length <= 10)
+    if (completions.length <= 10) {
       complete.style.width = (sel.clientWidth - 1) + "px";
+    }
 
     var done = false;
     function close() {
@@ -90,16 +98,16 @@ function autocomplete(editor) {  // Minimal event-handling wrapper.
     connect(sel, "keydown", function(event) {
       var code = event.keyCode;
       // Enter and space
-      if (code == 13 || code == 32) {event.stop(); pick();}
+      if (code == 13 || code == 32) { event.stop(); pick();}
       // Escape
-      else if (code == 27) {event.stop(); close(); editor.focus();}
-      else if (code != 38 && code != 40) {close(); editor.focus(); setTimeout(startComplete, 50);}
+      else if (code == 27) { event.stop(); close(); editor.focus();}
+      else if (code != 38 && code != 40) { close(); editor.focus(); setTimeout(startComplete, 50);}
     });
     connect(sel, "dblclick", pick);
 
     sel.focus();
     // Opera sometimes ignores focusing a freshly created node
-    if (window.opera) setTimeout(function(){if (!done) sel.focus();}, 100);
+    if (window.opera) { setTimeout(function(){if (!done) sel.focus();}, 100); }
     return true;
   }
 
@@ -112,38 +120,24 @@ function autocomplete(editor) {  // Minimal event-handling wrapper.
                   "if in instanceof new null return switch throw true try typeof var void while with").split(" ");
 
   function getCompletions(token, context) {
-    var found = [], start = token.string;
+    var found = [];
+    var start = token.string.replace(/"|'/g, '');
     function maybeAdd(str) {
       if (str.indexOf(start) == 0) found.push(str);
     }
-    function gatherCompletions(obj) {
-      if (typeof obj == "string") forEach(stringProps, maybeAdd);
-      else if (obj instanceof Array) forEach(arrayProps, maybeAdd);
-      else if (obj instanceof Function) forEach(funcProps, maybeAdd);
-      for (var name in obj) maybeAdd(name);
+    // If not, just look in the window object and any local scope
+    // (reading into JS mode internals to get at the local variables)
+    //for (var v = token.state.localVars; v; v = v.next) maybeAdd(v.name);
+    var regex = /\s+|\{|\}|\(|\)|"|\/|:|;|>|<|=|\]|\[|\.|\+|\-|,|\*|\'|\%/;
+    var base = editor.getValue() + " " + show.scripts;
+    var words = _.uniq(base.split(regex).filter(function(x) { return x.length > 1 }).sort());
+    var i_self = words.indexOf(token.string);
+    if(i_self !== -1) {
+      words.splice(i_self, 1);
     }
-
-    if (context) {
-      // If this is a property, see if it belongs to some object we can
-      // find in the current environment.
-      var obj = context.pop(), base;
-      if (obj.className == "variable")
-        base = window[obj.string];
-      else if (obj.className == "string")
-        base = "";
-      else if (obj.className == "atom")
-        base = 1;
-      while (base != null && context.length)
-        base = base[context.pop().string];
-      if (base != null) gatherCompletions(base);
-    }
-    else {
-      // If not, just look in the window object and any local scope
-      // (reading into JS mode internals to get at the local variables)
-      for (var v = token.state.localVars; v; v = v.next) maybeAdd(v.name);
-      gatherCompletions(window);
-      forEach(keywords, maybeAdd);
-    }
+    // Remove current string
+    for (var i = 0; i < words.length; i++) maybeAdd(words[i]);
+    console.log(found);
     return found;
   }
 }
