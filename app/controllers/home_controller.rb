6d7c81ebf42c3d93824bcd587fb81a82fbb8dcd9
Ashleigh Baumgardner <labaumgardner@gmail.com>
2012-03-20 16:36:33 -0700

    Cleaned up some assets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   def index
     @about = Faq.find_by_question('about')
     @faqs = Faq.visible.sort(:position).all
-    @primary = Network.primary.sort(:position).all
   end
 
   def admin
